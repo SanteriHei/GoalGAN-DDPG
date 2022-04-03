Add correct scaling to actors output. Add possibility for batch normalization for DDPG. Update other components accordingly

models/agent.py
@@ -37,6 +37,9 @@ class DDPGConfig:
     state_size: int 
         Defines the size of each state. The state MUST have 1-dimensional size,
         i.e. the tuple defining it's size has only 1 element.
+    action_range: int
+        The range for the actions. Should be symmetric,
+        i.e. if the range for actions is [-10, 10], set action-range to 10.
     actor_lr: float, Optional
         The learning rate used with the actor's optimizer. Default 0.0001
     critic_lr: float, Optional
@@ -52,9 +55,14 @@ class DDPGConfig:
         The maximum size of a memory buffer. Default 100 000
     batch_size: int, Optional
         The size of the sampled batch from the memory buffer. Default 128
+    actor_batch_norm: bool, Optional
+        Determines if the actor uses batch normalization between layers. Default False.
+    critic_batch_nor,: bool, Optional
+        Determines if the critic uses batch normalization between layers. Default False.
     '''
     action_size: int = _NOT_SET
     state_size: int = _NOT_SET
+    action_range: float = _NOT_SET
     actor_lr: float = 1e-4
     critic_lr: float = 1e-4
     weight_decay: float = 0.0
@@ -62,12 +70,14 @@ class DDPGConfig:
     gamma: float = 0.99
     buffer_size: int = int(1e5)
     batch_size: int = 128
+    actor_batch_norm: bool = False
+    critic_batch_norm: bool = False
 
     def __str__(self) -> str:
         '''Returns a simple table presentation of the config values'''
-        header = f"{'action-size':^11s}|{'state-size':^10s}|{'actor-lr':^8s}|{'critic-lr':^9s}|{'weight-decay':^12s}|{'tau':^7s}|{'gamma':^9s}|{'buffer-size':^11s}|{'batch-size':^10s}"
-        delim = f"{11*'-'}|{10*'-'}|{8*'-'}|{9*'-'}|{12*'-'}|{7*'-'}|{9*'-'}|{11*'-'}|{10*'-'}"
-        values = f"{self.action_size:^11d}|{self.state_size:^10d}|{self.actor_lr:^8.4f}|{self.critic_lr:^9.4f}|{self.weight_decay:^12.6f}|{self.tau:^7.5f}|{self.gamma:^9.4f}|{self.buffer_size:^11d}|{self.batch_size:^10d}"
+        header = f"{'action-size':^11s}|{'state-size':^10s}|{'action-range':^12s}|{'actor-lr':^8s}|{'critic-lr':^9s}|{'weight-decay':^12s}|{'tau':^7s}|{'gamma':^9s}|{'buffer-size':^11s}|{'batch-size':^10s}|{'actor-norm':^10s}|{'critic-norm':^11s}"
+        delim = f"{11*'-'}|{10*'-'}|{12*'-'}|{8*'-'}|{9*'-'}|{12*'-'}|{7*'-'}|{9*'-'}|{11*'-'}|{10*'-'}|{10*'-'}|{11*'-'}"
+        values = f"{self.action_size:^11d}|{self.state_size:^10d}|{self.action_range:^12.4f}|{self.actor_lr:^8.4f}|{self.critic_lr:^9.4f}|{self.weight_decay:^12.6f}|{self.tau:^7.5f}|{self.gamma:^9.4f}|{self.buffer_size:^11d}|{self.batch_size:^10d}|{str(self.actor_batch_norm):^10s}|{str(self.critic_batch_norm):^11s}"
         return f"{header}\n{delim}\n{values}"
 
 class DDPGAgent:    
@@ -90,23 +100,32 @@ class DDPGAgent:
         
         self._state_size: int = self._check_dim_and_unwrap(config.state_size)
         self._action_size: int = self._check_dim_and_unwrap(config.action_size)
+        self._action_range: int = config.action_range
         self._device: torch.device = torch.device(device) if isinstance(device, str) else device
 
         assert self._state_size != _NOT_SET, f"No state size was specified!"
         assert self._action_size != _NOT_SET, f"No action size was specified!"
-
+        assert self._action_range != _NOT_SET, f"No action range was specified!"
 
         #Actor network
-        self._actor_local = Actor(self._state_size, self._action_size).to(self._device)
-        self._actor_target = Actor(self._state_size, self._action_size).to(self._device)
+        self._actor_local = Actor(
+            self._state_size, self._action_size, 
+            self._action_range, use_norm=config.actor_batch_norm
+        ).to(self._device)
+
+        self._actor_target = Actor(
+            self._state_size, self._action_size,
+            self._action_range, use_norm=config.actor_batch_norm
+        ).to(self._device)
+        
         self._actor_optim = optim.Adam(
             self._actor_local.parameters(), 
             lr=config.actor_lr, weight_decay=config.weight_decay
         )
 
         #Critic network
-        self._critic_local = Critic(self._state_size, self._action_size).to(self._device)
-        self._critic_target = Critic(self._state_size, self._action_size).to(self._device)
+        self._critic_local = Critic(self._state_size, self._action_size, use_norm=config.critic_batch_norm).to(self._device)
+        self._critic_target = Critic(self._state_size, self._action_size, use_norm=config.critic_batch_norm).to(self._device)
         self._critic_optim = optim.Adam(
             self._critic_local.parameters(),
             lr=config.critic_lr, weight_decay=config.weight_decay
@@ -175,12 +194,7 @@ class DDPGAgent:
         self._buffer.append(state, action, reward, done, next_state)
 
 
-        ##if len(self._buffer) > self._buffer.batch_size:
-        ##    experiences = self._buffer.sample()
-        ##    self.learn(experiences)
-
-
-    def act(self, state: npt.NDArray, use_noise: bool = True, vrange: Tuple[float, float] = (0.0, 1.0)) -> npt.NDArray:
+    def act(self, state: npt.NDArray, use_noise: bool = True) -> npt.NDArray:
         '''
         Uses the current policy to create actions for the given state
 
@@ -191,13 +205,11 @@ class DDPGAgent:
         use_noise: bool, Optional
             Controls if additional noise is added to the 
             generated actions. Default True.
-        vrange: Tuple[int, int], Optional
-            Defines the range, where the values should be clipped to.
         Returns
         -------
         npt.NDArray
-            The generated actions, where value have been 
-            clipped to to be in range of values defined in the range parameter.
+            The generated actions, where values are in the (-action_range, action_range) 
+            interval. To set action range, see the constructor.
         '''
 
         state = torch.from_numpy(state).float().to(self._device)
@@ -214,8 +226,8 @@ class DDPGAgent:
 
         if use_noise:
             cpu_action += self._noise.sample()
+        return cpu_action.squeeze()
 
-        return np.clip(cpu_action, *vrange).squeeze()
 
 
     def log_losses_and_reset(self, global_step: Optional[int] = None) -> None:

models/rl.py
@@ -1,13 +1,11 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-import copy
+
 import numpy as np
-import random
+import copy
 
-from collections import deque, namedtuple
-
-from typing import Deque, Union, Tuple, Sequence
+from typing import Union, Tuple, Sequence
 import numpy.typing as npt
 
 
@@ -142,32 +140,49 @@ class Actor(nn.Module):
     ----------
     state_size: int
         The dimension of states
+    action_range: int
+        The range for the actions. Should be symmetric, 
+        i.e. if the range for actions is [-10, 10], set action-range to 10. 
     action_size: int
         The dimension of actions
     fc1_units: int, Optional
         The amount of units in the first hidden layer. Default 256
     fc2_units: int, Optional
         The amount of units in the second hidden layer. Default 256
+    use_norm: bool, Optional
+        Determines if batch normalization is used between layers. Default False.
     '''
-    def __init__(self, state_size: int, action_size: int, fc1_units: int = 256, fc2_units: int = 256) -> None:
+    def __init__(self, state_size: int, action_size: int, action_range: int, fc1_units: int = 256, fc2_units: int = 256, use_norm: bool = False) -> None:
         super(Actor, self).__init__()
-        self.layers = nn.ModuleList([
-            nn.Linear(state_size, fc1_units),
-            nn.ReLU(),
-            nn.Linear(fc1_units, fc2_units),
-            nn.ReLU(),
-            nn.Linear(fc2_units, action_size),
-            nn.Tanh()
-        ])
+
+        self._action_range: int = action_range
+        #self._layers = nn.ModuleList([
+        #    nn.Linear(state_size, fc1_units),
+        #    nn.ReLU(),
+        #    nn.Linear(fc1_units, fc2_units),
+        #    nn.ReLU(),
+        #    nn.Linear(fc2_units, action_size),
+        #    nn.Tanh()
+        #])
+
+        self._fc1 = nn.Linear(state_size, fc1_units)
+        self._norm1 = nn.LayerNorm(fc1_units) if use_norm else None
+
+        self._fc2 = nn.Linear(fc1_units, fc2_units)
+        self._norm2 = nn.LayerNorm(fc2_units) if use_norm else None
+
+        self._fc3 = nn.Linear(fc2_units, action_size)
+        
+        self._use_norm = use_norm
         self.reset()
 
     def reset(self) -> None:
         '''
         Resets the weights of the model by applying a initalization function
         '''
-        self.layers[0].weight.data.uniform_(*_hidden_init(self.layers[0]))
-        self.layers[2].weight.data.uniform_(*_hidden_init(self.layers[2]))
-        self.layers[4].weight.data.uniform_(-3e-3, 3e-3)
+        self._fc1.weight.data.uniform_(*_hidden_init(self._fc1))
+        self._fc2.weight.data.uniform_(*_hidden_init(self._fc2))
+        self._fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         '''
@@ -181,13 +196,22 @@ class Actor(nn.Module):
         Returns
         -------
         torch.Tensor
-            The resulting actions
+            The resulting actions, in range of (-action_range, action_range)
 
         '''
-        for layer in self.layers:
-            x = layer(x)
-        return x
-    
+
+        x = F.relu(self._fc1(x))
+        if self._use_norm:
+            x = self._norm1(x)
+        
+        x = F.relu(self._fc2(x))
+        if self._use_norm:
+            x = self._norm2(x)
+
+        #Tanh returns values between (-1,1). Multiply by the symmetric range to produce correct results
+        return torch.tanh(self._fc3(x))*self._action_range
+
+
 
 class Critic(nn.Module):
     '''
@@ -204,21 +228,32 @@ class Critic(nn.Module):
         The amount of units in the first hidden layer. Default 256
     fsc2_units: int, Optional
         The amout of units in the second hidden layer. Default 256
+    use_norm: bool, Optional
+        Determines if batch normalization is used between layers. Default False.
     '''
-    def __init__(self, state_size: int, action_size: int, fsc1_units: int = 256, fsc2_units: int = 256) -> None:
+    def __init__(self, state_size: int, action_size: int, fc1_units: int = 256, fc2_units: int = 256, use_norm: bool = False) -> None:
         super(Critic, self).__init__()
-        self._fsc1 = nn.Linear(state_size, fsc1_units)
-        self._fsc2 = nn.Linear(fsc1_units + action_size, fsc2_units)
-        self._fsc3 = nn.Linear(fsc2_units, 1)
+        
+        self._fc1 = nn.Linear(state_size, fc1_units)
+        self._norm1 = nn.LayerNorm(fc1_units) if use_norm else None
+
+        self._fc2 = nn.Linear(fc1_units + action_size, fc2_units)
+        self._norm2 = nn.LayerNorm(fc2_units) if use_norm else None
+        
+        self._fc3 = nn.Linear(fc2_units, 1)
+        
+        self._use_norm = use_norm
+
+
         self.reset()
 
     def reset(self) -> None:
         '''
         Resets the networks weights by applying initalization function
         '''
-        self._fsc1.weight.data.uniform_(*_hidden_init(self._fsc1))
-        self._fsc2.weight.data.uniform_(*_hidden_init(self._fsc2))
-        self._fsc3.weight.data.uniform_(-3e-3, 3e-3)
+        self._fc1.weight.data.uniform_(*_hidden_init(self._fc1))
+        self._fc2.weight.data.uniform_(*_hidden_init(self._fc2))
+        self._fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
         '''
@@ -236,10 +271,16 @@ class Critic(nn.Module):
         torch.Tensor
             The Q-value.
         '''
-        xs = F.relu(self._fsc1(state))
+        xs = F.relu(self._fc1(state))
+        if self._use_norm:
+            xs = self._norm1(xs)
+
         x = torch.cat((xs, action), dim=1)
-        x = F.relu(self._fsc2(x))
-        return self._fsc3(x)
+        x = F.relu(self._fc2(x))
+        if self._use_norm:
+            xs = self._norm2(x)
+        
+        return self._fc3(x)
 
 
 class OUNoise:
@@ -264,13 +305,16 @@ class OUNoise:
         sigma_min: float = 0.05, sigma_decay: float = 0.975, seed: Union[int, Sequence[int]] = None
     ) -> None: 
         self._mu = mu * np.ones(size)
-        self._theta = theta
-        self._sigma = sigma
-        self._sigma_min = sigma_min
-        self._sigma_decay = sigma_decay
+        self._theta: float = theta
+        self._sigma: float = sigma
+        self._sigma_min: float = sigma_min
+        self._sigma_decay:float = sigma_decay
         self._size = size
         self._state = None
         self._rng = np.random.default_rng() if seed is None else np.random.default_rng(seed)
+        #TODO: the variance + variance_ decay
+        #self._var: float = var
+        #self._variance_decay_rate: float = 0.999
         self.reset()    
 
     def reset(self) -> None:
@@ -293,7 +337,7 @@ class OUNoise:
             The sample from the distribution.
         '''
         assert self._state is not None, "Trying to call sample before setting the state (can be set by calling reset)"
-        dx = self._theta * (self._mu - self._state) + self._sigma * self._rng.standard_normal(self._size)
+        dx = self._theta * (self._mu - self._state) + self._sigma * self._rng.standard_normal(size=self._size)
         self._state += dx
         return self._state
 

train.py
@@ -94,20 +94,25 @@ def _update_or_eval_policy(
 
     tag = "update" if not eval_mode else "eval"
     
-    r = (env.action_space.low.min(), env.action_space.high.max())
     rewards = np.zeros((policy_iter_count*episode_count, ))
     env.eval = eval_mode
-    start_steps, update_after, update_every = 1000, 2000, 20
+    start_steps, update_after = 1000, 2000
     
     total_steps = 0
     for p in range(policy_iter_count):
         for ep in range(episode_count):
             state = env.reset()
             for ts in range(timestep_count):
-                #When eval mode is true, use noise is set to false.     
-                action = env.action_space.sample() if not eval_mode and ts*(ep+1) < start_steps else  agent.act(state, use_noise=not eval_mode, vrange=r)
-            
-                #action = agent.act(state, use_noise=not eval_mode)
+                  
+                if not eval_mode and total_steps < start_steps:
+                    action = env.action_space.sample()
+                else:
+                    #When eval mode is true, use noise is set to false and vice versa.   
+                    action = agent.act(state, use_noise=not eval_mode)
+
+                    if ( np.abs(np.max(np.abs(action)) - env.action_limits[1]) < 1e-3 ):
+                        _logger.debug(f"Action min: {np.min(action)}, max: {np.max(action)}")
+
                 next_state, reward, done = env.step(action)
                 
                 if not eval_mode:
@@ -127,28 +132,10 @@ def _update_or_eval_policy(
     _writer.add_scalar(f"reward/{tag}-avg-reward", avg_reward, global_step=global_step)
     if not eval_mode:
         agent.log_losses_and_reset(global_step)
-
-    if eval_mode:
+    else:
         global_rewards[global_step] = avg_reward
         return np.clip(env.achieved_goals_counts/(policy_iter_count*episode_count), 0,1)
 
-
-
-def test_update(agent: DDPGAgent, env: MazeEnv, goals: Sequence[npt.NDArray], episode_count: int, timestep_count: int) -> npt.NDArray:
-    
-    r = (env.action_space.low.min(), env.action_space.high.max())
-    returns = np.zeros(goals.shape)
-    for i in range(goals.shape[0]):
-        env.goals = [goals[i]]
-        for ep in range(episode_count):
-            state = env.reset()
-            for ts in range(timestep_count):
-                action = agent.act(state, use_noise=False, vrange=r)
-                next_state, reward, done = env.step(action)
-                if done:
-                    break
-        returns[i] = np.clip(env.achieved_goals_counts[0]/(episode_count), 0, 1)    
-    return returns
 
 def _observe_states(agent: DDPGAgent, env: MazeEnv, goals: Sequence[npt.NDArray], episode_count: int, timestep_count: int) -> npt.NDArray:
     '''
@@ -259,7 +246,7 @@ def _initialize_gan(
 
     rng = np.random.default_rng()
     #Create the initial goals.
-    goals = rng.uniform(*env.limits, size=(goal_count, env.goal_size))
+    goals = rng.uniform(*env.obs_limits, size=(goal_count, env.goal_size))
     visited_positions = _observe_states(agent, env, goals, episode_count, timestep_count)
     labels = np.ones((max(visited_positions.shape), ))
     gan.train(torch.from_numpy(visited_positions), labels, gan_iter_count, global_step=0)
@@ -372,16 +359,16 @@ def train(
     consecutive_iters = 0
     
     #Use points that are close to the agent as the first set of old goals
-    #old_goals = torch.clip(
-    #    torch.from_numpy(env.agent_pos.astype(np.float32)) + 0.5*torch.normal(0, 1, (old_goal_count, env.goal_size)), *env.limits
-    #).to(gan.device)
-    old_goals = None
+    old_goals = torch.clip(
+        torch.from_numpy(env.agent_pos.astype(np.float32)) + 0.5*torch.normal(0, 1, (old_goal_count, env.goal_size)), *env.obs_limits
+    ).to(gan.device)
+    #old_goals = None
 
     for i in range(iter_count):
         _logger.info(f"OUTER ITERATION {i+1}")
     
         #---------- Create Goals -------------
-        gan_goals = _create_goals(gan,  gan_goal_count, env.limits)
+        gan_goals = _create_goals(gan,  gan_goal_count, env.obs_limits)
         goals = gan_goals if old_goals is None else torch.cat([gan_goals, utils.sample_tensor(old_goals, old_goal_count)])
         
         env.goals = goals.detach().cpu().numpy()
@@ -410,7 +397,7 @@ def train(
         else:
             consecutive_iters = 0
         
-        if consecutive_iters > 20:
+        if consecutive_iters > 10:
             _logger.critical(f"[iter: {i}] {consecutive_iters} consecutive iters with 0-labels. Aborting!")
             break
 
@@ -426,7 +413,7 @@ def train(
 
         # ---------- Display goals ---------------
         utils.display_agent_and_goals(
-            env.agent_pos, goals.detach().cpu().numpy(), returns, env.limits,
+            env.agent_pos, goals.detach().cpu().numpy(), returns, env.obs_limits,
             rmin, rmax, filepath=f"images/goals_iter_{i}.svg",
             pos_label="Agent position", title=f"Iteration {i}"
         )