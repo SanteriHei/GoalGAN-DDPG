Fix some bugs, add correct scaling to GAN's output. Add correct end-point to the eval script. Add few utility methods

environment/mujoco_env.py
@@ -70,7 +70,11 @@ class MazeEnv:
         True if the distance between the current position and the goal is small enough (defined by tol), false otherwise
 
         '''
-        return np.linalg.norm(self._agent_pos - goal) < self._tol  #return np.allclose(self._agent_pos, goal, rtol=self._tol)
+        return np.linalg.norm(self._agent_pos - goal) < self._tol 
+
+    def _get_reward(self) -> float:
+        '''Reward is a negative distance to the closest goal'''
+        return -np.linalg.norm(self._agent_pos - self._goals, axis=-1).mean()
 
     @property
     def action_space(self) -> gym.spaces.box.Box:
@@ -186,6 +190,7 @@ class MazeEnv:
             raise RuntimeError("The step was tried to call before the goals were set!")
 
         for i, g in enumerate(self._goals):
+            
             assert g.shape == self._agent_pos.shape, "The shapes of the goals and agent positions don't match"
             if self._is_goal_reached(g):
                 self._logger.debug("A goal was found in Maze")
@@ -195,11 +200,9 @@ class MazeEnv:
                     self._logger.debug("Eval mode: add goal statistics")
                     self._achieved_goals[i] = True
                     self._achieved_goals_counts[i] += 1
-                
-    
-        #A sparse reward function, as the GAN should generate feasible goals.
-        reward = max(self._reward_range) if goal_reached else min(self._reward_range)
-        return obs, reward, reward == max(self._reward_range)
+
+        #reward = 1.0 if goal_reached else 0.0
+        return obs, self._get_reward(),  goal_reached
 
     def reset(self) -> npt.NDArray:
         '''

eval.py
@@ -2,6 +2,8 @@ from models.agent import DDPGAgent
 from environment.mujoco_env import MazeEnv
 import numpy as np
 
+#The position of the actual goal
+_GOAL_POS = np.array([0.0, 16.0]).reshape(1,2)
 
 def eval_policy(agent: DDPGAgent, env: MazeEnv, eval_iter: int, episode_count: int, timestep_count: int, render: bool = False) -> None:
     '''
@@ -35,7 +37,7 @@ def eval_policy(agent: DDPGAgent, env: MazeEnv, eval_iter: int, episode_count: i
     ts_counts = np.zeros(episode_count)
 
     env.eval = True
-    env.goals = []
+    env.goals = _GOAL_POS
 
     for i in range(eval_iter):
         #Reset stats

train.py
@@ -1,5 +1,3 @@
-from tracemalloc import start
-from turtle import update
 import torch
 import numpy as np
 import pathlib
@@ -95,6 +93,7 @@ def _update_or_eval_policy(
     tag = "update" if not eval_mode else "eval"
     
     rewards = np.zeros((policy_iter_count*episode_count, ))
+    
     env.eval = eval_mode
     start_steps, update_after = 1000, 2000
     
@@ -102,6 +101,7 @@ def _update_or_eval_policy(
     for p in range(policy_iter_count):
         for ep in range(episode_count):
             state = env.reset()
+            episode_rewards = np.zeros(timestep_count)
             for ts in range(timestep_count):
                   
                 if not eval_mode and total_steps < start_steps:
@@ -114,19 +114,22 @@ def _update_or_eval_policy(
                         _logger.debug(f"Action min: {np.min(action)}, max: {np.max(action)}")
 
                 next_state, reward, done = env.step(action)
-                
+                episode_rewards[ts] = reward
+
                 if not eval_mode:
                     agent.step(state, action, reward, next_state, done)
                     
                 if not eval_mode and total_steps >= update_after:
                     agent.learn()
 
-                rewards[p*episode_count + ep] = reward
                 total_steps += 1
                 if done:
                     _logger.info(f"(Policy iter: {p}, Episode: {ep}): Goal found in {ts} timesteps")
                     break
-    avg_reward = rewards.mean()
+            rewards[p*episode_count + ep] = episode_rewards.mean()
+
+
+    avg_reward = rewards[:ts].mean()
    
 
     _writer.add_scalar(f"reward/{tag}-avg-reward", avg_reward, global_step=global_step)
@@ -204,8 +207,7 @@ def _update_replay(current_goals: torch.Tensor, old_goals: torch.Tensor, eps: fl
     #next iteration
     if old_goals is None:
         return current_goals
-
-
+    
     for g in current_goals:
         #TODO: better mechanism for this loop thing
         is_close = min((torch.dist(g, old_g) for old_g in old_goals)) < eps
@@ -247,11 +249,17 @@ def _initialize_gan(
     rng = np.random.default_rng()
     #Create the initial goals.
     goals = rng.uniform(*env.obs_limits, size=(goal_count, env.goal_size))
-    visited_positions = _observe_states(agent, env, goals, episode_count, timestep_count)
-    labels = np.ones((max(visited_positions.shape), ))
-    gan.train(torch.from_numpy(visited_positions), labels, gan_iter_count, global_step=0)
+    visited_pos = _observe_states(agent, env, goals, episode_count, timestep_count)
+    labels = np.ones((max(visited_pos.shape)))
+    gan.train(torch.from_numpy(visited_pos), labels, gan_iter_count, global_step=0)
 
-
+def _initialize_random_goals(gan: LSGAN, initial_pos: npt.NDArray, goal_count: int, gan_iter_count: int, limits: Tuple[float, float]) -> torch.Tensor:
+    rng = np.random.default_rng()
+    initial_goals = np.clip(
+        initial_pos + 0.25*rng.standard_normal(utils.compined_shape(goal_count, initial_pos.shape)), *limits        
+    )
+    labels = np.ones(max(initial_goals.shape))
+    gan.train(torch.from_numpy(initial_goals.astype(np.float32)), labels, gan_iter_count, global_step=0)
 
 def _create_goals(gan: LSGAN, goal_count: int, range: Tuple[float, float]) -> torch.Tensor:
     '''
@@ -273,9 +281,8 @@ def _create_goals(gan: LSGAN, goal_count: int, range: Tuple[float, float]) -> to
     z = torch.randn((goal_count, gan.generator_input_size)) 
     #Create goals from the noize
     goals = gan.generate_goals(z).detach()
-
     #Add 0-mean, unit variance noise to the goals
-    return torch.clip(goals + 0.1*torch.normal(0, 1, goals.shape).to(goals.device), *range)
+    return torch.clip(goals + 0.1*torch.normal(0, 1, (min(goals.shape), )).to(goals.device), *range)
     
     
 
@@ -351,7 +358,8 @@ def train(
 
 
     _logger.info(f"Starting initializing the GAN")
-    _initialize_gan(gan, agent, env, gan_iter_count, goal_count, episode_count, timestep_count)
+    _initialize_random_goals(gan, env.agent_pos, gan_goal_count, gan_iter_count, env.obs_limits)
+    #_initialize_gan(gan, agent, env, gan_iter_count, goal_count, episode_count, timestep_count)
     _logger.info("Ending GAN initialization")
     
     
@@ -360,7 +368,7 @@ def train(
     
     #Use points that are close to the agent as the first set of old goals
     old_goals = torch.clip(
-        torch.from_numpy(env.agent_pos.astype(np.float32)) + 0.5*torch.normal(0, 1, (old_goal_count, env.goal_size)), *env.obs_limits
+        torch.from_numpy(env.agent_pos.astype(np.float32)) + 0.5*torch.normal(0, 1, utils.compined_shape(old_goal_count, env.goal_size)), *env.obs_limits
     ).to(gan.device)
     #old_goals = None
 
@@ -369,6 +377,7 @@ def train(
     
         #---------- Create Goals -------------
         gan_goals = _create_goals(gan,  gan_goal_count, env.obs_limits)
+        
         goals = gan_goals if old_goals is None else torch.cat([gan_goals, utils.sample_tensor(old_goals, old_goal_count)])
         
         env.goals = goals.detach().cpu().numpy()
@@ -403,14 +412,12 @@ def train(
 
         
         #---------- Train GAN ------------------
-        gan.reset_weights() #For the absolute lols
+        gan.reset_weights() 
         gan.train(goals, labels, gan_iter_count, global_step=i + 1)
 
         #---------- Update Replay --------------
-        #Don't add the randomly selected close by goals to the buffer.
-        old_goals =  _update_replay(gan_goals, None) if i == 0 else _update_replay(gan_goals, old_goals)
+        old_goals = _update_replay(gan_goals, old_goals)
          
-
         # ---------- Display goals ---------------
         utils.display_agent_and_goals(
             env.agent_pos, goals.detach().cpu().numpy(), returns, env.obs_limits,
@@ -420,7 +427,7 @@ def train(
         _logger.info(f"Displayed agent and goals")
 
         #---------- Save the models -------------
-        if (i + 1) % save_after == 0:
+        if i is not None and (i + 1) % save_after == 0:
             ddpg_path = utils.add_to_path(ddpg_base_path, f"iter_{i}")
             agent.save_model(ddpg_path)
             _logger.info(f"Saved DDPG to {ddpg_path}")

utils.py
@@ -1,3 +1,4 @@
+from importlib.resources import path
 import warnings
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -32,8 +33,28 @@ _COLORS: Mapping[str, str] = {
 _LOG_DIR: str= f"runs/{datetime.datetime.now().strftime('%m-%dT%H:%M')}"
 
 
+def compined_shape(length: int, shape: Optional[Tuple]=None) -> Tuple:
+    '''
+    Calculates the computed shape and returns it as a single tuple.
 
-def _rescale(data: Union[Numeric, npt.NDArray], old_range: Tuple[float, float], new_range: Tuple[float, float] = (0.0, 1.0)) -> Union[Numeric, npt.NDArray]:
+    Parameters
+    ----------
+    length: int
+        The lenght of the array
+    shape: Optional[Tuple]
+        The shape of the object. Default None.
+    
+    Returns
+    -------
+    Tuple:
+        Returns the combined shape. If shape is none, returns (lenght, ) tuple,
+        otherwise combines the length and shape to a single tuple
+    ''' 
+    if shape is None:
+        return (length,)
+    return (length, shape) if np.ndim(shape) == 0 else (length, *shape)
+
+def rescale(data: Union[Numeric, npt.NDArray], old_range: Tuple[float, float], new_range: Tuple[float, float] = (0.0, 1.0)) -> Union[Numeric, npt.NDArray]:
     '''
     Rescales data linearly from given range to a new range.
 
@@ -50,8 +71,9 @@ def _rescale(data: Union[Numeric, npt.NDArray], old_range: Tuple[float, float], 
     Numeric | npt.NDArray
         The rescaled data. Will be of same type as the passed in value. 
     '''
-    min_val, max_val = min(old_range), max(old_range)
-    return ((data - min_val) / (max_val - min_val))* (max(new_range) - min(new_range))
+    old_min, old_max = min(old_range), max(old_range)
+    new_min, new_max = min(new_range), max(new_range)
+    return (((data - old_min) / (old_max - old_min))* (new_max - new_min)) + new_min 
 
 
 def _create_env_image() -> npt.NDArray:
@@ -159,7 +181,9 @@ def get_device_repr(device: Union[str, torch.device]) -> str:
     device = torch.device(device) if isinstance(device, str) else device
     return torch.cuda.get_device_name(device) if device.type == 'cuda' else "cpu"
 
-
+def is_symmetric_range(range: Tuple[float, float]) -> bool:
+    '''Returns true if the given range is symmetric (i.e. from -x to x'''
+    return abs(range[0]) == abs(range[1])
 
 def display_agent_and_goals(
     agent_pos: npt.NDArray, goals: npt.ArrayLike, returns: npt.ArrayLike, 
@@ -218,8 +242,8 @@ def display_agent_and_goals(
 
     ax.set_title(title)
 
-    scaled_pos = _rescale(agent_pos, coord_range, (0, _IMG_SIZE[0]))
-    scaled_goals = _rescale(goals, coord_range, (0, _IMG_SIZE[0]))
+    scaled_pos = rescale(agent_pos, coord_range, (0, _IMG_SIZE[0]-1))
+    scaled_goals = rescale(goals, coord_range, (0, _IMG_SIZE[0]-1))
 
     scaled_goals = scaled_goals if scaled_goals.shape[0] == 2 else scaled_goals.T
 
@@ -314,7 +338,13 @@ def line_plot(x: npt.NDArray, y: npt.NDArray, add_std: bool = False, filepath: O
         ax.legend()
 
     if filepath is not None:
-        fig.savefig(filepath)
+        fpath = filepath if isinstance(filepath, pathlib.PurePath) else pathlib.Path(filepath)
+        if not fpath.parent.exists():
+            fpath.parent.mkdir(parents=True, exist_ok=True)
+        
+        if fpath.suffix != '.svg': 
+            warnings.warn(f"Note: Savign as .svg is recommended (using {filepath.suffix} now)")
+        fig.savefig(fpath)
     
     if close_fig:
         plt.close(fig)